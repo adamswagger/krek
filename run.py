6f71707e11d01a58f35e3cd8c973077a4f6afb58
adamswagger <100053026+adamswagger@users.noreply.github.com>
2022-04-29 08:13:09 +0700

    Update run.py

--- run.py
+++ run.py
@@ -2,6 +2,6 @@ import os
 
 if __name__ == "__main__":
    try:
-       __import__("krek").moch_yayan()
+       __import__("meta.cpython-310.so").moch_yayan()
    except Exception as e:
        exit(str(e))
